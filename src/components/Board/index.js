c4d3689e12162c5f5e67732a9827e8c36257ea7e
MGG275 <125992797+MGG275@users.noreply.github.com>
2023-03-21 09:38:19 -0400

    act

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Square from '../Square'
-import './style.css'
+import React from "react";
+import Square from "../Square";
+import "./style.css";
 
-export default function Board ({ arrayValues, handleClicParametro}) {
-
-	return (
-		<div className='board'>
-			{ arrayValues.map((value,index) => 
-				<Square i={index} 
-				valueSquare = { value } 
-				handleClic = {handleClicParametro}/>
-				) 
-			}
-		</div>
-	)
-};
+export default function Board({ arrayValues, handleClicParametro }) {
+  return (
+    <div className="board">
+      {arrayValues.map((value, index) => (
+        <Square
+          i={index}
+          valueSquare={value}
+          handleClic={handleClicParametro}
+        />
+      ))}
+    </div>
+  );
+}
--- src/components/Winner/index.js
+++ src/components/Winner/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
-export default function Winner({ ganador , handleReset}) {
-
-	return (
-		<div id='div-winner'>
-			<h1> { ganador } </h1>
-			<button onClick={handleReset}>Reset</button>
-		</div>
-	)
+export default function Winner({ ganador, handleReset }) {
+  return (
+    <div id="div-winner">
+      <h1> {ganador} </h1>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
